b03935c94fff6b6492939a1050f0aa8c5c429758
Nomsplease <Nomsplease.code@gmail.com>
2020-09-03 15:26:57 -0400

    Enable ipv6 on emulation testing

--- run.sh
+++ run.sh
@@ -6,4 +6,4 @@ qemu-system-arm -cpu arm1176 -m 256 \
   -nographic \
   -append "dwc_otg.lpm_enable=0 root=/dev/sda2 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait" \
   -drive "file=src/workspace/2020-02-13-raspbian-buster-0.0.5.img,index=0,media=disk,format=raw" \
-  -net user,hostfwd=tcp::22222-:22,hostfwd=tcp::8888-:80 -net nic
+  -net user,ipv4=on,ipv6=on,hostfwd=tcp::22222-:22,hostfwd=tcp::8888-:80 -net nic
