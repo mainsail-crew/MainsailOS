4bb2cb2ba6fb0d8ce446ad38b5b75bc3edf88efa
Mikkel Schmidt <mikkel.schmidt@gmail.com>
2023-02-09 00:24:38 +0100

    Fix klipperscreen issues and build scripts

--- build-pi64.sh
+++ build-pi64.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-if [[ -f "./src/config.local" ]]; then
-	rm "./src/config.local"
+if [[ -f "./src/config" ]]; then
+	rm "./src/config"
 fi
 cat "./config/default" >> ./src/config
 cat "./config/raspberry/default" >> ./src/config
 cat "./config/raspberry/rpi64" >> ./src/config
 source ./src/config
-echo "Downloading image..."
-aria2c -d ./src/image --seed-time=0 $DOWNLOAD_URL_IMAGE
+IMGCOUNT=$(ls ./src/image/*raspios-bullseye-arm64*.img.xz | wc -l)
+if [ $IMGCOUNT -eq 0 ]; then
+	echo "Downloading image..."
+	rm -f ./src/image/*
+	aria2c -d ./src/image --seed-time=0 $DOWNLOAD_URL_IMAGE
+fi
+exit 0
 pushd ./src
 sudo bash -x ./build_dist
 popd
